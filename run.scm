1206d3aaf062dadbacf29c6663a9214bf2267c91
Olgierd Grzyb <kontakt@olgierd.me>
2017-03-10 21:31:08 +0100

    implement trivial routing

--- run.scm
+++ run.scm
@@ -1,6 +1,41 @@
+(use-modules (ice-9 regex))
 (use-modules (web server))
-(define (handler request request-body)
-  (values '((content-type . (text/plain)))
-          "Hello World"))
+(use-modules (web request)
+             (web response)
+             (web uri))
 
-(run-server handler)
+(define (request-path-components request)
+  (split-and-decode-uri-path (uri-path (request-uri request))))
+
+(define (trivial-response text)
+  (values '((content-type . (text/plain)))
+          text))
+
+(define (hello-controller request request-body)
+  (trivial-response "Hello, World!"))
+
+(define (not-found request request-body)
+  (values (build-response #:code 404)
+          (string-append "Resource not found: "
+                         (uri->string (request-uri request)))))
+
+(define *routes*
+  `(("^/$" ,hello-controller)))
+
+(define (match? route uri)
+  (string-match (car route) uri))
+
+(define (find-controller request request-body)
+  (let ((uri (uri-path (request-uri request))))
+    (define (find-controller-iter routes)
+      (let ((route (car routes)))
+        (cond
+          ((null? routes) not-found)
+          ((match? route uri) (cadr route))
+          (else (find-controller-iter (cdr routes))))))
+    (find-controller-iter *routes*)))
+
+(define (main-handler request request-body)
+  ((find-controller request request-body) request request-body))
+
+(run-server main-handler)
